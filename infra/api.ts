34ebeeb739753e9d6e9db84bf1872bcb999cbf32
Tommy Carter <tommyjr@gmail.com>
2024-08-22 11:33:42 -0400

    Rest of API CRUD

--- infra/api.ts
+++ infra/api.ts
@@ -11,4 +11,9 @@ export const api = new sst.aws.ApiGatewayV2('Api', {
   },
 });
 
+api.route('GET /notes', 'packages/functions/src/list.main');
+
 api.route('POST /notes', 'packages/functions/src/create.main');
+api.route('GET /notes/{id}', 'packages/functions/src/get.main');
+api.route('PUT /notes/{id}', 'packages/functions/src/update.main');
+api.route('DELETE /notes/{id}', 'packages/functions/src/delete.main');
