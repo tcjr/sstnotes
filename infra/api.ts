a5b62de71a55d3cb4c93ce68bfd0f238d49d8ebd
Tommy Carter <tommyjr@gmail.com>
2024-08-22 13:45:50 -0400

    Added billing via stripe

--- infra/api.ts
+++ infra/api.ts
@@ -1,12 +1,12 @@
-import { table } from './storage';
+import { secret, table } from './storage';
 
 // Create the API
 export const api = new sst.aws.ApiGatewayV2('Api', {
   transform: {
     route: {
       handler: {
-        // makes the table available to all our routes
-        link: [table],
+        // makes the table and stripe secret available to all our routes
+        link: [table, secret],
       },
       args: {
         // tells our API that we want to use AWS_IAM across all our routes
@@ -22,3 +22,5 @@ api.route('POST /notes', 'packages/functions/src/create.main');
 api.route('GET /notes/{id}', 'packages/functions/src/get.main');
 api.route('PUT /notes/{id}', 'packages/functions/src/update.main');
 api.route('DELETE /notes/{id}', 'packages/functions/src/delete.main');
+
+api.route('POST /billing', 'packages/functions/src/billing.main');
