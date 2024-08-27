456b184a9be3ddd76accc9773d91f46acf0f5cd4
Tommy Carter <tommyjr@gmail.com>
2024-08-27 10:42:02 -0400

    Added note edit/delete

--- packages/react-frontend/src/Routes.tsx
+++ packages/react-frontend/src/Routes.tsx
@@ -4,6 +4,7 @@ import NotFound from './containers/NotFound.tsx';
 import Login from './containers/Login.tsx';
 import Signup from './containers/Signup.tsx';
 import NewNote from './containers/NewNote.tsx';
+import Notes from './containers/Notes.tsx';
 
 export default function Links() {
   return (
@@ -11,9 +12,10 @@ export default function Links() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/notes/new" element={<NewNote />} />{' '}
+      <Route path="/notes/new" element={<NewNote />} />
+      <Route path="/notes/:id" element={<Notes />} />
       {/* Finally, catch all unmatched routes */}
-      <Route path="*" element={<NotFound />} />;{' '}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
