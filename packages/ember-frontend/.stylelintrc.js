56cdcec9ff947fa1bc8d586c60f3a2d882075351
Tommy Carter <tommyjr@gmail.com>
2024-09-03 16:46:10 -0500

    Created stand-alone ember-app

---
+++ packages/ember-frontend/.stylelintrc.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'no-descending-specificity': null,
+  },
+};
