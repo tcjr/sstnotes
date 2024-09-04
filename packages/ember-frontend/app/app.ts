0bc9e9a0b57b14de0847e6965d118e5fa09fb305
Tommy Carter <tommyjr@gmail.com>
2024-09-04 13:32:18 -0500

    Added and configured Amplify

--- packages/ember-frontend/app/app.ts
+++ packages/ember-frontend/app/app.ts
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-frontend/config/environment';
+import { Amplify } from 'aws-amplify';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default class App extends Application {
@@ -11,3 +12,27 @@ export default class App extends Application {
 }
 
 loadInitializers(App, config.modulePrefix);
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'notes',
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
--- packages/ember-frontend/app/config/environment.d.ts
+++ packages/ember-frontend/app/config/environment.d.ts
@@ -3,6 +3,21 @@
  *    import config from 'ember-frontend/config/environment'
  */
 declare const config: {
+  cognito: {
+    REGION: string;
+    USER_POOL_ID: string;
+    APP_CLIENT_ID: string;
+    IDENTITY_POOL_ID: string;
+  };
+  s3: {
+    BUCKET: string;
+    REGION: string;
+  };
+  apiGateway: {
+    URL: string;
+    REGION: string;
+  };
+
   environment: string;
   modulePrefix: string;
   podModulePrefix: string;
