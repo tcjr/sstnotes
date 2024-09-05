53e5df21d1e3060ad646fcf46da47237837e673e
Tommy Carter <tommyjr@gmail.com>
2024-09-05 10:04:59 -0500

    Added create note w/upload

--- packages/ember-frontend/app/router.ts
+++ packages/ember-frontend/app/router.ts
@@ -9,5 +9,8 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('login');
   this.route('signup');
+  this.route('notes', function () {
+    this.route('new');
+  });
   this.route('not-found', { path: '/*path' });
 });
